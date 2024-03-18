bf3ed7841606bb660aa582f61ccfb3b3156ff2ac
JasonHsu <53471811+Jason-xy@users.noreply.github.com>
2024-03-18 13:11:39 +0000

    deploy KuzcoChat

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -544,7 +544,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>KuzcoChat</div>
             <div className={styles["sub-title"]}>
               github.com/Yidadaa/ChatGPT-Next-Web
             </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,9 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "KuzcoChat",
+  description:
+    "The distributed GPU cluster for LLM inference on Solana(kuzco.xyz)",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +23,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "KuzcoChat",
     statusBarStyle: "default",
   },
 };
